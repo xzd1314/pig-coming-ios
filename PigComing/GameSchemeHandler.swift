9b0f37bbb9fee1e2490fd4a7d52ed84565844ce3
xzd1314 <xzd1314@users.noreply.github.com>
2026-09-05 22:36:16 +0800

    fix: GameSchemeHandler 方法加 @objc 标记

--- PigComing/GameSchemeHandler.swift
+++ PigComing/GameSchemeHandler.swift
@@ -1,7 +1,54 @@
 import Foundation
 import WebKit
 
-class GameSchemeHandler: NSObject, WKURLSchemeHandler {
-    func urlSchemeHandler(_ handler: WKURLSchemeHandler, start urlSchemeTask: WKURLSchemeTask) {}
-    func urlSchemeHandler(_ handler: WKURLSchemeHandler, stop urlSchemeTask: WKURLSchemeTask) {}
+final class GameSchemeHandler: NSObject, WKURLSchemeHandler {
+
+    weak var gameLauncher: GameLauncher?
+
+    private static let mimeMap: [String: String] = [
+        "html": "text/html", "htm": "text/html",
+        "js": "application/javascript",
+        "css": "text/css",
+        "json": "application/json",
+        "png": "image/png",
+        "jpg": "image/jpeg", "jpeg": "image/jpeg",
+        "gif": "image/gif",
+        "svg": "image/svg+xml",
+        "mp3": "audio/mpeg",
+        "wav": "audio/wav",
+        "ogg": "audio/ogg",
+        "mp4": "video/mp4",
+        "webm": "video/webm",
+        "woff": "font/woff",
+        "woff2": "font/woff2",
+        "ttf": "font/ttf",
+        "txt": "text/plain",
+        "xml": "application/xml",
+    ]
+
+    @objc func urlSchemeHandler(_ handler: WKURLSchemeHandler, start urlSchemeTask: WKURLSchemeTask) {
+        let url = urlSchemeTask.request.url!
+        var path = url.path
+        if path.hasPrefix("/") { path.removeFirst() }
+        if path.isEmpty { path = "index.html" }
+
+        if let data = gameLauncher?.getFile(path) {
+            let ext = (path as NSString).pathExtension.lowercased()
+            let mime = Self.mimeMap[ext] ?? "application/octet-stream"
+            let response = URLResponse(url: url, mimeType: mime,
+                                        expectedContentLength: data.count, textEncodingName: nil)
+            urlSchemeTask.didReceive(response)
+            urlSchemeTask.didReceive(data)
+            urlSchemeTask.didFinish()
+        } else {
+            let response = URLResponse(url: url, mimeType: "text/plain",
+                                        expectedContentLength: 0, textEncodingName: "utf-8")
+            urlSchemeTask.didReceive(response)
+            urlSchemeTask.didFinish()
+        }
+    }
+
+    @objc func urlSchemeHandler(_ handler: WKURLSchemeHandler, stop urlSchemeTask: WKURLSchemeTask) {
+        // 同步从内存读取，不需要特殊处理
+    }
 }
