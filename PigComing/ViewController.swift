b18a2162abd09f1eb0a336553e5949cddf5f67ae
xzd1314 <xzd1314@users.noreply.github.com>
2026-09-06 16:23:18 +0800

    fix: 移除不存在的udpManager.stop调用

--- PigComing/ViewController.swift
+++ PigComing/ViewController.swift
@@ -10,7 +10,6 @@ class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDele
 
     deinit {
         wsServer.stop()
-        udpManager.stop()
     }
 
     override func viewDidLoad() {
